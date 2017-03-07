bbf0396fdeeb6c1b7734efee552e74de46e3e578
Jack Wong <jackwong3101@yahoo.com>
2017-03-07 10:14:11 +0800

    updated Enumerables
    Solved bugs when no block is given - it should return an Enumerator.

--- enumerable.rb
+++ enumerable.rb
@@ -1,50 +1,84 @@
 module Enumerable
   def my_each
     0.upto(self.length-1) do |item|
-      yield self[item]
+      yield self[item] if block_given?
+    end
+    if block_given?
+      self
+    else
+      self.to_enum
     end
   end
 
   def my_each_with_index
     0.upto(self.length-1) do |item|
-      yield self[item],item
+      yield self[item],item if block_given?
+    end
+    if block_given?
+      self
+    else
+      self.to_enum
     end
   end
 
   def my_select
     arr = []
-    self.my_each {|item| arr << item if yield item}
-    arr
+    self.my_each {|item| arr << item if (yield item if block_given?)}
+    if block_given?
+      arr
+    else
+      self.to_enum
+    end
   end
 
   def my_all?
     bool = true
-    self.my_each {|item| bool = false if !yield item}
-    bool
+    self.my_each {|item| bool = false if !(yield item if block_given?)}
+    if block_given?
+      bool
+    else
+      self.to_enum
+    end
   end
 
   def my_any?
     bool = false
-    self.my_each {|item| bool = true if yield item}
-    bool
+    self.my_each {|item| bool = true if (yield item if block_given?)}
+    if block_given?
+      bool
+    else
+      self.to_enum
+    end
   end
 
   def my_none?
     bool = true
-    self.my_each {|item| bool = false if yield item}
-    bool
+    self.my_each {|item| bool = false if (yield item if block_given?)}
+    if block_given?
+      bool
+    else
+      self.to_enum
+    end
   end
 
   def my_count
     counter = 0
-    self.my_each {|item| counter += 1 if yield item}
-    counter
+    self.my_each {|item| counter += 1 if (yield item if block_given?)}
+    if block_given?
+      counter
+    else
+      self.to_enum
+    end
   end
 
   def my_map
     arr = []
-    self.my_each {|item| arr << (yield item)}
-    arr
+    self.my_each {|item| arr << (yield item if block_given?)}
+   if block_given?
+      arr
+    else
+      self.to_enum
+    end
   end
 #Proc version of my_map
 =begin
@@ -65,21 +99,24 @@ module Enumerable
      end
     arr
   end
-
 #test:
 ps = Proc.new{|item| item + 1}
 p [1,2,3,4,5].my_map(&ps)
 =end
 
   def my_inject(n=0)
-    self.my_each {|item| n = (yield n,item) }
-    n
+    self.my_each {|item| n = (yield n,item if block_given?) }
+   if block_given?
+      n
+    else
+      self.to_enum
+    end
   end
-=begin *test my_inject using method below*
+=begin
+*test my_inject using method below*
   def multiply_els(arr)
     arr.my_inject(1) {|a,item| a*item}
   end
-
 p multiply_els([4,5,4])
 =end
 
